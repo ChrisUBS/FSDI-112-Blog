f382290678ca3650bccb0a67f5a8b0dd2b8d87a0
Christian Bonilla <christianurielbonillasuarez@gmail.com>
2025-03-21 23:15:32 -0700

    Final Report

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        "title", "subtitle", "body", "status"
+    ]
+
+admin.site.register(Post, PostAdmin)
--- posts/urls.py
+++ posts/urls.py
@@ -3,6 +3,10 @@ from posts import views
 
 urlpatterns = [
     path("", views.PostListView.as_view(), name="list"),
+    path("draft/", views.DraftPostListView.as_view(), name="draft"),
+    path("archived/", views.ArchivedPostListView.as_view(), name="archived"),
+    path("<int:pk>/edit/", views.PostUpdateView.as_view(), name="edit"),
+    path("<int:pk>/delete/", views.PostDeleteView.as_view(), name="delete"),
     path("<int:pk>/", views.PostDetailView.as_view(), name="detail"),
     path("new/", views.PostCreateView.as_view(), name="new"),
 ]
